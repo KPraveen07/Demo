eea904bd198ccf4c52637276b8ddefa0bda1c411
KPraveen07 <114064842+KPraveen07@users.noreply.github.com>
2022-09-24 15:14:26 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+package day2;
+
+// program to print welcome message
+
+public class Welcome 
+{
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Welcome to Java");
+
+	}
+
+}
